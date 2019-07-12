673ff47825719da37f4380cd2a2b58c2c9c21148
Brad Bartlett <bradbartlett1@me.com>
2019-07-12 11:24:31 -0400

    way harder than the first

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,4 +3,25 @@ class GuestsController < ApplicationController
   def index
     @guests = Guest.all
   end
+
+  def new
+    @guest = Guest.new
+  end 
+
+  def edit
+    @guest = Guest.find(params[:id])
+  end 
+
+  def show
+    @guest = Guest.find(params[:id])
+  end 
+
+  def create
+    @guest = Guest.new(guest_params)
+    if @guest.save
+      redirect_to guest_path(@guest)
+    else 
+      render :new
+    end 
+  end 
 end
